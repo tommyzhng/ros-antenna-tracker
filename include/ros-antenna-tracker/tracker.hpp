d0779c553982914bbe494b587a6fec7c3c2d4764
tommyzhng <tommyz2005@gmail.com>
2024-12-25 12:45:38 -0500

    update declarations

--- include/ros-antenna-tracker/tracker.hpp
+++ include/ros-antenna-tracker/tracker.hpp
@@ -2,7 +2,8 @@
 #define ROS_ANTENNA_TRACKER_TRACKER_HPP
 
 #include <ros/ros.h>
-#include <sensor_msgs/NavSatFix.msg>
+#include <sensor_msgs/NavSatFix.h>
+#include <geometry_msgs/Point.h>
 #include <eigen3/Eigen/Dense>
 
 class Tracker
@@ -11,13 +12,18 @@ public:
     Tracker(ros::NodeHandle &nh);
     ~Tracker();
 private:
-    void RecieveDronePositionCb();
-    void ConvertEf2Bf();
+    void RecieveDronePositionCb(sensor_msgs::NavSatFix msg);
+    void ConvertEf2Lf();
     void CalculateError();
     void PublishAngles();
 
-    Eigen::Vector3f curLLA_;  // current global position of the drone
-    Eigen::Vector3f initLLA_; // initial global position of the drone at boot
+    bool altitudeInitialized_{false};
+
+    ros::Subscriber dronePositionSub_;
+    ros::Publisher anglesPub_;
+
+    Eigen::Vector3f droneLLA_;  // current global position of the drone
+    Eigen::Vector3f antennaLLA_{0, 0, 0}; // initial global position of the drone at boot
     Eigen::Vector3f localError_;
 };
 
--- src/tracker.cpp
+++ src/tracker.cpp
@@ -1,9 +1,25 @@
-#include <tracker.hpp>
+#include "tracker.hpp"
 
 Tracker::Tracker(ros::NodeHandle &nh)
 {
+    float antenna_longitude, antenna_lattitude;
+    nh.param("antenna_longitude", antenna_longitude);
+    nh.param("antenna_lattitude", antenna_lattitude);
+    antennaLLA_ << antenna_longitude, antenna_lattitude, 0;
 
+    dronePositionSub_ = nh.subscribe("/mavros/global_position/global", 1, &Tracker::RecieveDronePositionCb, this);
+    anglesPub_ = nh.advertise<geometry_msgs::Point>("/antenna_tracker/angles", 1);
 }
 Tracker::~Tracker()
+{ 
+
+}
+void Tracker::RecieveDronePositionCb(sensor_msgs::NavSatFix msg)
 {
+    // initialize the altitude of the antenna
+    if (!altitudeInitialized_){
+        antennaLLA_.z() = msg.altitude;
+        altitudeInitialized_ = true;
+    }
+    droneLLA_ << msg.longitude, msg.latitude, msg.altitude;
 }
